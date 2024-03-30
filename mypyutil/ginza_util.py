6eb8ce7f61de331a0cf83dc2d3e02e4ec2020449
curoa <yuuseitori@gmail.com>
2024-03-30 21:46:08 +0900

    update

--- mypyutil/ginza_util.py
+++ mypyutil/ginza_util.py
@@ -17,7 +17,7 @@ Token.set_extension("tag_0", getter=lambda token: get_tag(token, 0))
 Token.set_extension("tag_1", getter=lambda token: get_tag(token, 1))
 Token.set_extension("tag_2", getter=lambda token: get_tag(token, 2))
 Token.set_extension("tag_3", getter=lambda token: get_tag(token, 3))
-# print('token.tag_1', token._.tag_1) # debug
+# print('token._.tag_1', token._.tag_1) # debug
 
 # pos, dep list https://qiita.com/kei_0324/items/400f639b2f185b39a0cf
 
