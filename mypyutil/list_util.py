267a878ac28e5d668cea7d5fb2fb1780a4545c73
curoa <yuuseitori@gmail.com>
2024-04-13 14:53:57 +0900

    impl list_util.argmin

--- mypyutil/list_util.py
+++ mypyutil/list_util.py
@@ -1,6 +1,17 @@
+import math
+
 def list_get(l, i, default=None):
     if i < len(l):
         return l[i]
     else:
         return default
+
+def argmin(l):
+    best_i = 0
+    best_v = math.inf
+    for i, v in enumerate(l):
+        if v < best_v:
+            best_i = i
+            best_v = v
+    return best_i
 
