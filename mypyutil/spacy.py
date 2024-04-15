83af7812010d4aa055e1035586ea089ade164189
curoa <yuuseitori@gmail.com>
2024-04-15 14:59:15 +0900

    add nlp_pipe_with_cache

--- mypyutil/spacy.py
+++ mypyutil/spacy.py
@@ -15,3 +15,22 @@ nlp.pipeline = [
 exclude = ["ner", "morphologizer", "compound_splitter", ] # "tok2vec" is needed for bunsetu_recognizer
 nlp = spacy.load("ja_ginza", exclude=exclude)
 doc_list = nlp.pipe(text_list)
+
+
+from spacy.tokens import DocBin
+
+def nlp_pipe_with_cache(nlp, func_get_text_list, fpath):
+    docbin = DocBin(store_user_data=True) #TODO save as cheetsheet
+    if not os.path.exists(fpath):
+        text_list = func_get_text_list()
+        for doc in tqdm(nlp.pipe(text_list, n_process=8), total=len(text_list)):
+            for sent in doc.sents:
+                print('sent', sent) # debug
+            docbin.add(doc)
+        obj = docbin.to_bytes()
+        pickle.dump(obj, open(fpath, "wb"))
+    obj = pickle.load(open(fpath, "rb"))
+    docbin.from_bytes(obj)
+    doc_list = list(docbin.get_docs(nlp.vocab))
+    return doc_list
+
