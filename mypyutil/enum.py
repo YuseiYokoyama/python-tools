6aa950326729773e1f2f31b82026e30e679e2274
curoa <yuuseitori@gmail.com>
2024-08-21 02:04:43 +0900

    fix enum

--- mypyutil/enum.py
+++ mypyutil/enum.py
@@ -16,13 +16,8 @@ class Speaker(Enum):
         if type(value) == Speaker:
             return value
         if type(value) == str:
-            return conv[value]
+            # ref. https://docs.python.org/ja/3/library/enum.html#enum.EnumType.__members__
+            return Speaker.__members__[value]
         raise ValueError(f"can not make Speaker from {value}")
 
-    # Speaker.__members__ # ref. https://docs.python.org/ja/3/library/enum.html#enum.EnumType.__members__
-
-conv = { #NOTE write out of class to avoid Speaker is not defined
-        "OP": Speaker.OP,
-        "CU": Speaker.CU,
-        }
 
